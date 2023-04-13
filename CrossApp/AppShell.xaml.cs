e7ca00f44237f3053459be2266c7b73ec3cbe8de
westtly25 <westtly25@gmail.com>
2023-04-13 19:14:03 +0300

    Add .net maui app & images of products

---
+++ CrossApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CrossApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CrossApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CrossApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
